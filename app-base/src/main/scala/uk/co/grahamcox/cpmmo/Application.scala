8e2e8aa1db7d3c7fbfbf25de84f7f06f79cd24d7
Graham Cox <graham@grahamcox.co.uk>
2014-06-11 23:55:36 +0100

    Application base with logging and configured to be assembled and run

---
+++ app-base/src/main/scala/uk/co/grahamcox/cpmmo/Application.scala
@@ -0,0 +1,11 @@
+package uk.co.grahamcox.cpmmo
+
+import org.eintr.loglady.Logging
+
+/**
+ * The main Application endpoint
+ */
+object Application extends App with Logging {
+    log.info("Starting Application")
+    log.info("Stopping Application")
+}
